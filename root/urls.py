16038287ac80032705af99fd7c611af47c89d74e
bunyodbeek <bunyodbekshukuraliyev453@gmail.com>
2025-12-01 18:03:06 +0500

    add commit

--- root/urls.py
+++ root/urls.py
@@ -1,12 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import include, path
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('apps.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),  # set_language
 ]
 
+urlpatterns += i18n_patterns(
+    path('admin/', admin.site.urls),
+    path('', include('apps.urls')),     # BU YERGA i18n_patterns JOYLASHADI ❗
+)
+
+# static files
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
