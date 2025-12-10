b291bd9ad45bf6b71f4fe393d59f4edffc2886de
islombek575 <hoshimovislombek023@gmail.com>
2025-12-10 14:09:28 +0500

    add commit

--- apps/models/payments.py
+++ apps/models/payments.py
@@ -1,4 +1,6 @@
-from apps.models.base import CreatedBaseModel
+from datetime import timedelta
+
+from dateutil.relativedelta import relativedelta
 from django.db.models import (
     CASCADE,
     SET_NULL,
@@ -13,19 +15,71 @@ from django.db.models.fields import (
     CharField,
     DateTimeField,
     DecimalField,
-    IntegerField,
-)
+    IntegerField, )
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+
+from apps.models.base import CreatedBaseModel
+
+
+class SubscriptionPlan(Model):
+    class PeriodChoices(TextChoices):
+        MONTHLY = 'monthly', "1 oylik"
+        QUARTERLY = 'quarterly', "3 oylik"
+        YEARLY = 'yearly', "1 yillik"
+
+
+    price = DecimalField(max_digits=10, decimal_places=2)
+    period = CharField(max_length=20, choices=PeriodChoices.choices)
+
+    is_active = BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.get_period_display()}"
+
+    def get_expiry_date(self, start_date):
+        if self.period == 'monthly':
+            return start_date + relativedelta(months=1)
+        elif self.period == 'quarterly':
+            return start_date + relativedelta(months=3)
+        elif self.period == 'yearly':
+            return start_date + relativedelta(years=1)
+        return 0
+
+    def period_days(self) -> int:
+        if self.period == 'monthly':
+            return 30
+        elif self.period == 'quarterly':
+            return 90
+        elif self.period == 'yearly':
+            return 365
+        return 0
+
 
 
 class Subscription(Model):
     user = OneToOneField('apps.UserProfile', CASCADE, related_name='subscription', verbose_name=_("User"))
+    plan = ForeignKey('apps.SubscriptionPlan', CASCADE, related_name='subscriptions')
     start_date = DateTimeField(_("Start Date"), auto_now_add=True)
     end_date = DateTimeField(_("End Date"))
     is_active = BooleanField(_("Is Active"), default=True)
-    auto_renew = BooleanField(_("Auto Renew"), default=True)
-    price = DecimalField(_("monthly payment price"), max_digits=10, decimal_places=2, default=67000.00)
+
+    def save(self, *args, **kwargs):
+        if not self.end_date and self.plan:
+            start = self.start_date or timezone.now()
+            self.end_date = self.plan.get_expiry_date(start)
+        super().save(*args, **kwargs)
+
+
+    def period(self):
+        return self.plan.period_days()
+
+    def days_remaining(self) -> int:
+        if not self.is_active or not self.end_date:
+            return 0
+        delta: timedelta = self.end_date - timezone.now()
+        return max(0, delta.days)
+
 
     class Meta:
         verbose_name = _("Subscription")
@@ -34,12 +88,6 @@ class Subscription(Model):
 
     def __str__(self):
         return f"{self.user.name} - Premium"
-
-    def days_remaining(self):
-        if not self.is_active:
-            return 0
-        delta = self.end_date - timezone.now()
-        return max(0, delta.days)
 
 
 class Payment(CreatedBaseModel):
